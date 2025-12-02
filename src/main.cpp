c1a8c5db2858284ed18ad83d66905cbd0d789f75
Bavin <bavinbav5@gmail.com>
2025-12-02 17:50:54 +0300

    sorry uploded wrong file here is the working one

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,49 @@
-#include <Arduino.h>
+const int trigPin = 5, echopin = 18;
+volatile unsigned long start_time = 0;
+volatile unsigned long pulse_width = 0;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 
-// put function declarations here:
-int myFunction(int, int);
+void ultra_sonic_task(void *pvParameters) {
 
+
+  while (1) {
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    vTaskDelay(100/portTICK_PERIOD_MS);
+  }
+}
+void IRAM_ATTR echo_ISR() {
+  bool echo_stat = digitalRead(echopin);
+  portENTER_CRITICAL_ISR(&timerMux);
+  if (echo_stat==HIGH) {
+    start_time = micros();
+  }
+  else {
+   pulse_width = micros()-start_time;
+  }
+  portEXIT_CRITICAL_ISR(&timerMux);
+}
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echopin, INPUT);
+  attachInterrupt(echopin, echo_ISR, CHANGE);
+  Serial0.begin(115200);
+  xTaskCreatePinnedToCore(ultra_sonic_task, "ultraSonicTask", 4096, NULL, 2, NULL, 0);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  char buff [70];
+  portENTER_CRITICAL_ISR(&timerMux);
+  uint16_t distance_raw=pulse_width;
+  portEXIT_CRITICAL_ISR(&timerMux);
+  uint16_t distance_data=distance_raw/58;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  sprintf(buff, "Distance in cm: %d",distance_data);
+  Serial0.println(buff);
+  delay(100);
+
+
+
 }
